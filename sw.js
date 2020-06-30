160bb0dd5afa89398e3d4208cd4f0f3d1047395b
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2020-07-01 02:33:08 +0530

    Local

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 importScripts('/js/workbox/workbox-sw.js');
-
+workbox.setConfig({
+  modulePathPrefix: '/js/workbox/'
+});
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'script' ||
                    request.destination === 'style'  ||
